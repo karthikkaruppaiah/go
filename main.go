5ae70f63e90a30c92f6a949c4bdd59c0a68029e9
karthik k <kar123thik45@gmail.com>
2019-05-13 23:21:43 -0700

    check commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello, world\n")
+	if 1 == 1 {
+		fmt.Println("1")
+		fmt.Println(2)
+	}
+	Temp()
+}
+
+// Temp not sure why
+func Temp() {
+	FileName := "123"
+	fmt.Println(FileName)
+}
